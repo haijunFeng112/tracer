2f84da41c6b080eca225780c7e18452fc8ce6499
haijunFeng112 <39263655+haijunFeng112@users.noreply.github.com>
2020-06-12 19:42:49 +0800

    登录注册完结

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-from web.views import account
+from web.views import account,home
 
 urlpatterns = [
     url(r'^register/$',account.register,name='register' ),
     url(r'^send/sms/$',account.sendsms,name='sendsms' ),
+    url(r'^login/sms/$',account.loginsms,name='loginsms'),
+    url(r'image/code',account.image_code,name='image_code'),
+    url(r'^login/$',account.login,name='login'),
+    url(r'^logout/$',account.logout,name='logout'),
+    url(r'^index/$',home.index,name='index')
 ]
--- web/views/account.py
+++ web/views/account.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render,HttpResponse
-from web.forms.account import RegisterModelForm,SendSmSForm
+
+from django.shortcuts import render,HttpResponse,redirect
+
+from web import models
+from web.forms.account import RegisterModelForm,SendSmSForm,LoginSmSForm,LoginForm
 from django.http import JsonResponse
 
 def register(request):
     """注册"""
-    form = RegisterModelForm()
-    return render(request,"register.html",{'form':form})
+    if request.method == 'GET':
+        form = RegisterModelForm()
+        return render(request,"register.html",{'form':form})
+
+    form = RegisterModelForm(data=request.POST)
+    if form.is_valid():
+        #验证通过，写入数据库(密码要密文)
+        form.save()
+
+        return JsonResponse({'status':True,'data':'/login/'})
+
+    return JsonResponse({'status':False,'error':form.errors})
 
 def sendsms(request):
     """发送短信"""
@@ -15,3 +28,58 @@ def sendsms(request):
         return JsonResponse({'status':True})
 
     return JsonResponse({'status':False,'error':form.errors})
+
+def loginsms(request):
+    """短信登录"""
+    if request.method =="GET":
+        form = LoginSmSForm()
+        return render(request,"loginSmS.html",{'form':form})
+    form = LoginSmSForm(data=request.POST)
+    if form.is_valid():
+        user_object = form.cleaned_data['mobile_phone']
+
+        #把用户名写到session中
+        request.session['user_id'] = user_object.id
+        #用户输入正确，登录成功
+        return JsonResponse({"status":True,"data":"/index/"})
+    return JsonResponse({'status':False,'error':form.errors})
+
+def login(request):
+    """用户名和密码登录"""
+    if request.method == 'GET':
+        form = LoginForm(request)
+        return render(request,'login.html',{'form':form})
+    form = LoginForm(request,request.POST)
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+
+        from django.db.models import Q
+
+        user_object = models.UserInfo.objects.filter(Q(email=username) | Q(mobile_phone = username)).filter(
+            password=password).first()
+        if user_object:
+            request.session['user_id'] = user_object.id
+            request.session.set_expiry(60*60*24*14)
+            #用户名密码正确
+            return redirect("index")
+        form.add_error('username','用户名或密码错误')
+    return render(request,'login.html',{'form':form})
+
+def logout(request):
+    request.session.flush()
+    return redirect('index')
+def image_code(request):
+    """生成图片验证码"""
+    from io import BytesIO
+    from Utils.image_code import check_code
+
+    image_object,code = check_code()
+
+    request.session['image_code'] = code #将验证码放入session中
+    request.session.set_expiry(60) #主动修改session的过期时间为60秒
+
+    stream = BytesIO()
+    image_object.save(stream,'png')
+
+    return HttpResponse(stream.getvalue())
